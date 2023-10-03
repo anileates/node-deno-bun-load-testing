ed7c5e655c84a0fff10a1a22ae8dbbd5cfdbbb1c
unknown <atesanilerdem@gmail.com>
2023-10-03 12:09:24 +0300

    Initial

---
+++ deno-app/index.ts
@@ -0,0 +1,9 @@
+import express, { Application } from 'express';
+
+const app: Application = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('Server is running on 3000'));
